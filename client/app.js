3d598b2d8ae70b28bca00250dcba78d425db4262
rasyidzkun <rasyidzkun@gmail.com>
2023-01-16 10:34:50 +0700

    update app

--- client/app.js
+++ client/app.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://dull-lime-python-sari.cyclic.app/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
